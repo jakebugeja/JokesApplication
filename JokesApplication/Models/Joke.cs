230d31edebdaa1498759c774f088a360e0554d5f
Jake Bugeja <Jake.Bugeja.b42775@mcast.edu.mt>
2021-07-28 18:59:42 +0200

    Added three properties to class joke

--- JokesApplication/Models/Joke.cs
+++ JokesApplication/Models/Joke.cs
@@ -7,5 +7,8 @@ namespace JokesApplication.Models
 {
     public class Joke
     {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
     }
 }
